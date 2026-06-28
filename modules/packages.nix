126cf70522b0cd971138a4b24be2bce9538bcf58
willie6666 <willie35212736@gmail.com>
2026-06-28 22:15:18 +0800

    update

--- modules/packages.nix
+++ modules/packages.nix
@@ -18,7 +18,10 @@
   };
 
   programs.noisetorch.enable = true;
-  programs.gamescope.enable  = true;
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
   programs.gamemode.enable   = true;
 
   programs.virt-manager.enable = true;
